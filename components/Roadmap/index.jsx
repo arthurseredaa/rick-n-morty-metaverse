9a2e0ee75876bc000a0b5cc5521fbd09e3510321
arthurseredaa <neonbetcy@gmail.com>
2022-01-07 22:28:38 +0200

    Finish roadmap

--- components/Roadmap/index.jsx
+++ components/Roadmap/index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useEffect, useRef } from 'react';
 import { useOnScreen } from '../../hooks/useOnScreen';
 import styles from './roadmap.module.css';
@@ -14,7 +15,50 @@ export const Roadmap = ({ setCurrentPage }) => {
 
   return (
     <div ref={ref} className={styles.container}>
-      roadmap
+      <h3 className={styles.title}>Roadmap</h3>
+
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <Image
+            src="/images/morty-portal.jpg"
+            alt=""
+            width="500"
+            height="400"
+          />
+        </div>
+        <div className={styles.column}>
+          <h4>Phase 1</h4>
+          <h4>Preparation</h4>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+          quis qui, rerum incidunt voluptates magnam soluta iusto fugit esse
+          consectetur dignissimos eos sequi omnis repellat quod laboriosam
+          possimus minus, iure aliquam laudantium sunt? Illo id et quae dolorem
+          sed vitae rerum, odit consequatur, laborum excepturi, voluptatibus
+          dolorum nesciunt possimus iste.
+        </div>
+      </div>
+
+      <div className={styles.row}>
+        <div className={styles.column}>
+          <h4>Phase 2</h4>
+          <h4>NFT Giveaway</h4>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis
+          quis qui, rerum incidunt voluptates magnam soluta iusto fugit esse
+          consectetur dignissimos eos sequi omnis repellat quod laboriosam
+          possimus minus, iure aliquam laudantium sunt? Illo id et quae dolorem
+          sed vitae rerum, odit consequatur, laborum excepturi, voluptatibus
+          dolorum nesciunt possimus iste.
+        </div>
+
+        <div className={styles.column}>
+          <Image
+            src="/images/rick-portal.jpg"
+            alt=""
+            width="500"
+            height="400"
+          />
+        </div>
+      </div>
     </div>
   );
 };
