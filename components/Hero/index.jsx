b0661013d21660336c37eec58a3c1cd7e277e0f4
arthurseredaa <neonbetcy@gmail.com>
2022-01-20 20:23:28 +0200

    Change hero image

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { scroller } from 'react-scroll/modules';
 import { Button } from '../_common/Button';
@@ -12,19 +13,13 @@ export const Hero = () => {
 
   return (
     <div className={styles.container}>
-      <video
-        className={styles.video_bg}
-        autoPlay
-        loop
-        muted
-      >
-        <source
-          src="/videos/bg.mp4"
-          type="video/mp4"
-        />
+      <video className={styles.video_bg} autoPlay loop muted>
+        <source src="/videos/bg.mp4" type="video/mp4" />
       </video>
       <div className={styles.video_overlay} />
-      <Image src="/images/hero.jpg" width="500" height="500" alt="" />
+      <motion.div className={styles.hero_wrapper} whileInView={{transform: ['rotate(10)', 'rotate(-10)']}}>
+        <Image src="/images/hero.png" width="300" height="300" alt="" />
+      </motion.div>
       <Button onClick={handleClick}>Explore</Button>
     </div>
   );
